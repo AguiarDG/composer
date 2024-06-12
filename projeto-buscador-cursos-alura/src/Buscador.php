c40f59cd729316f03594b982c2721dcb338f43cc
AguiarDG <aguiardg90@gmail.com>
2024-06-12 15:54:32 -0300

    Adicionando teste de PHPUnit e formatando codigo no padrao PSR12

--- projeto-buscador-cursos-alura/src/Buscador.php
+++ projeto-buscador-cursos-alura/src/Buscador.php
@@ -5,39 +5,38 @@ namespace AguiarDG\BuscadorDeCursos;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-class Buscador 
+class Buscador
 {
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+    /**
+     * @var Crawler
+     */
+    private $crawler;
 
-  /**
-   * @var ClientInterface
-   */
-  private $httpClient;
-  /**
-   * @var Crawler
-   */
-  private $crawler;
-  
-  public function __construct(ClientInterface $httpClient, Crawler $crawler) 
-  {
-    $this->httpClient = $httpClient;
-    $this->crawler = $crawler;
-  }
-
-  public function buscar(string $url) : array {
-    $response = $this->httpClient->request('GET', $url);
-
-    $html = $response->getBody();
-
-    $this->crawler->addHtmlContent($html);
-
-    $elementoCursos = $this->crawler->filter('span.card-curso__nome');
-    $cursos = [];
-
-    foreach ($elementoCursos as $elemento) {
-      $cursos[] = $elemento->textContent;
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
     }
 
-    return $cursos;
-  }
-}
+    public function buscar(string $url): array
+    {
+        $response = $this->httpClient->request('GET', $url);
 
+        $html = $response->getBody();
+
+        $this->crawler->addHtmlContent($html);
+
+        $elementoCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementoCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+}
